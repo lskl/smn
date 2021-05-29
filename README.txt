4ac6e8ec3f2a66b3e70a9f2aaee80cf0c1fd007a
Luis <luis.miranda@posteo.net>
2021-05-29 17:18:12 +0200

    Actualizados enlaces al SMN; Creada función lib/io para leer archivos de climatología diaria

--- README.txt
+++ README.txt
@@ -11,6 +11,13 @@ Los datos primarios se encuentran en formato TXT. Cada archivo incluye un encabe
 
 No todos los días tienen datos, y no todas las líneas tienen mediciones de las cuatro variables.
 
+Los datos originales se pueden descargar desde la página del Sistema Meteorológico Nacional: https://smn.conagua.gob.mx/es/climatologia/informacion-climatologica/informacion-estadistica-climatologica
+
+Ejemplo de archivo con datos diarios:
+https://smn.conagua.gob.mx/tools/RESOURCES/Diarios/8030.txt
+
+
+
 --------------------------------------------------------------
 
 En este repositorio se considera que los datos están en el directorio: "../datos". Los datos primarios en formato TXT deben ester en "/datos/datos primarios/Total de estaciones".
---
+++ lib/io/cdiaria.py
@@ -0,0 +1,23 @@
+## ----------------------------------------------------------------
+## Funciones para leer los datos de los archivos de climatología
+## diaria del servicio meteorológico nacional mexicano
+## ----------------------------------------------------------------
+## Funciones en este archivo:
+## leer_cdiaria( filename, path='' )
+## ----------------------------------------------------------------
+## 
+## 
+## ----------------------------------------------------------------
+'''
+leer_cdiaria()
+
+Lee un archivo de texto con datos de climatología diaria de acuerdo
+a la definición del Sistema Meteorológico Nacional Mexicano,
+descrita en: 
+https://smn.conagua.gob.mx/tools/RESOURCES/estacion/EstacionesClimatologicas.pdf
+
+'''
+
+def leer_cdiaria( filename, path='' ):
+
+    return 0
